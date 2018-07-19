339994540bbbd8fe50dccf421b468679605e5337
lwkchan <laurachan205@gmail.com>
2018-07-19 18:43:47 +0100

    Implement server side search

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
-const isSearched = searchTerm => item =>
-item.title.toLowerCase().includes(searchTerm.toLowerCase()) // returns true if the item details match the search term
-
 class App extends Component {
   constructor(props){
     super(props);
@@ -18,21 +15,38 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
     };
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  componentDidMount(){
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm)
   }
 
   setSearchTopStories(result){
     this.setState({ result })
   }
 
-  componentDidMount(){
-    const { searchTerm } = this.state;
-
+  fetchSearchTopStories(searchTerm) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
+  }
+
+  onSearchChange(event){
+    const newSearchTerm = event.target.value
+    this.setState({ searchTerm: newSearchTerm })
+    this.fetchSearchTopStories(newSearchTerm)
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state
+    this.fetchSearchTopStories(searchTerm)
+    event.preventDefault()
   }
 
   onDismiss(id){
@@ -44,46 +58,50 @@ class App extends Component {
     })
   }
 
-  onSearchChange(event){
-    this.setState({ searchTerm: event.target.value })
-  }
-
   render() {
     const { result, searchTerm } = this.state;
-    if(!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
           <Search
             value={searchTerm}
             onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
           >
             Search
           </Search>
-          <Table
+          { result &&
+            <Table
             list={result.hits}
-            pattern={searchTerm}
             onDismiss={this.onDismiss}
           />
+        }
         </div>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children}
+const Search = ({
+  value,
+  onChange,
+  onSubmit,
+  children
+}) =>
+  <form onSubmit={onSubmit}>
     <input
     type="text"
     value={value}
     onChange={onChange}
     />
+    <button type="submit">
+      {children}
+    </button>
   </form>
 
 const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
-    {list.filter(isSearched(pattern)).map(item =>
+    {list.map(item =>
       <div key={item.objectID} className="table-row">
         <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
